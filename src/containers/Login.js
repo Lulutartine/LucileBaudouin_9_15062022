08f7ae0092d8a51c9c5ce253d1fdf387666c5011
Lucile <lucile.baudouin@gmail.com>
2022-06-23 10:29:48 -0400

    Authentification bug fixing

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -4,19 +4,18 @@ export let PREVIOUS_LOCATION = ''
 
 // we use a class so as to test its methods in e2e tests
 export default class Login {
-  constructor({ document, localStorage, onNavigate, PREVIOUS_LOCATION, store }) {
+  constructor({ document, localStorage, onNavigate, PREVIOUS_LOCATION, firestore }) {
     this.document = document
     this.localStorage = localStorage
     this.onNavigate = onNavigate
     this.PREVIOUS_LOCATION = PREVIOUS_LOCATION
-    this.store = store
+    this.firestore = firestore
     const formEmployee = this.document.querySelector(`form[data-testid="form-employee"]`)
     formEmployee.addEventListener("submit", this.handleSubmitEmployee)
     const formAdmin = this.document.querySelector(`form[data-testid="form-admin"]`)
     formAdmin.addEventListener("submit", this.handleSubmitAdmin)
   }
   handleSubmitEmployee = e => {
-    e.preventDefault()
     const user = {
       type: "Employee",
       email: e.target.querySelector(`input[data-testid="employee-email-input"]`).value,
@@ -24,50 +23,47 @@ export default class Login {
       status: "connected"
     }
     this.localStorage.setItem("user", JSON.stringify(user))
-    this.login(user)
-      .catch(
-        (err) => this.createUser(user)
-      )
-      .then(() => {
-        this.onNavigate(ROUTES_PATH['Bills'])
-        this.PREVIOUS_LOCATION = ROUTES_PATH['Bills']
-        PREVIOUS_LOCATION = this.PREVIOUS_LOCATION
-        this.document.body.style.backgroundColor="#fff"
-      })
-
+    const userExists = this.checkIfUserExists(user)
+    if (!userExists) this.createUser(user)
+    e.preventDefault()
+    this.onNavigate(ROUTES_PATH['Bills'])
+    this.PREVIOUS_LOCATION = ROUTES_PATH['Bills']
+    PREVIOUS_LOCATION = this.PREVIOUS_LOCATION
+    this.document.body.style.backgroundColor="#fff"
   }
 
   handleSubmitAdmin = e => {
-    e.preventDefault()
     const user = {
       type: "Admin",
-      email: e.target.querySelector(`input[data-testid="employee-email-input"]`).value,
-      password: e.target.querySelector(`input[data-testid="employee-password-input"]`).value,
+      email: e.target.querySelector(`input[data-testid="admin-email-input"]`).value,
+      password: e.target.querySelector(`input[data-testid="admin-password-input"]`).value,
       status: "connected"
     }
     this.localStorage.setItem("user", JSON.stringify(user))
-    this.login(user)
-      .catch(
-        (err) => this.createUser(user)
-      )
-      .then(() => {
-        this.onNavigate(ROUTES_PATH['Dashboard'])
-        this.PREVIOUS_LOCATION = ROUTES_PATH['Dashboard']
-        PREVIOUS_LOCATION = this.PREVIOUS_LOCATION
-        document.body.style.backgroundColor="#fff"
-      })
+    const userExists = this.checkIfUserExists(user)
+    if (!userExists) this.createUser(user)
+    e.preventDefault()
+    this.onNavigate(ROUTES_PATH['Dashboard'])
+    this.PREVIOUS_LOCATION = ROUTES_PATH['Dashboard']
+    PREVIOUS_LOCATION = this.PREVIOUS_LOCATION
+    document.body.style.backgroundColor="#fff"
   }
 
   // not need to cover this function by tests
-  login = (user) => {
-    if (this.store) {
-      return this.store
-      .login(JSON.stringify({
-        email: user.email,
-        password: user.password,
-      })).then(({jwt}) => {
-        localStorage.setItem('jwt', jwt)
+  checkIfUserExists = (user) => {
+    if (this.firestore) {
+      this.firestore
+      .user(user.email)
+      .get()
+      .then((doc) => {
+        if (doc.exists) {
+          console.log(`User with ${user.email} exists`)
+          return true
+        } else {
+          return false
+        }
       })
+      .catch(error => error)
     } else {
       return null
     }
@@ -75,21 +71,18 @@ export default class Login {
 
   // not need to cover this function by tests
   createUser = (user) => {
-    if (this.store) {
-      return this.store
+    if (this.firestore) {
+      this.firestore
       .users()
-      .create({data:JSON.stringify({
+      .doc(user.email)
+      .set({
         type: user.type,
-        name: user.email.split('@')[0],
-        email: user.email,
-        password: user.password,
-      })})
-      .then(() => {
-        console.log(`User with ${user.email} is created`)
-        return this.login(user)
+        name: user.email.split('@')[0] 
       })
+      .then(() => console.log(`User with ${user.email} is created`))
+      .catch(error => error)
     } else {
       return null
     }
   }
-}
+} 
