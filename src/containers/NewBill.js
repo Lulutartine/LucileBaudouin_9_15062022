29390f99e1a01c0999f1a258b86702cdc608f960
Lucile <lucile.baudouin@gmail.com>
2022-06-23 10:31:36 -0400

    NewBill document bug fixing

--- src/containers/NewBill.js
+++ src/containers/NewBill.js
@@ -1,44 +1,56 @@
+
 import { ROUTES_PATH } from '../constants/routes.js'
 import Logout from "./Logout.js"
 
 export default class NewBill {
-  constructor({ document, onNavigate, store, localStorage }) {
+  constructor({ document, onNavigate, firestore, localStorage }) {
     this.document = document
     this.onNavigate = onNavigate
-    this.store = store
+    this.firestore = firestore
     const formNewBill = this.document.querySelector(`form[data-testid="form-new-bill"]`)
     formNewBill.addEventListener("submit", this.handleSubmit)
     const file = this.document.querySelector(`input[data-testid="file"]`)
     file.addEventListener("change", this.handleChangeFile)
     this.fileUrl = null
     this.fileName = null
-    this.billId = null
     new Logout({ document, localStorage, onNavigate })
   }
+
+  alertExtension = e => {
+    window.alert('Choose a jpg, jpeg, or png format')
+  }
+
   handleChangeFile = e => {
-    e.preventDefault()
     const file = this.document.querySelector(`input[data-testid="file"]`).files[0]
+    const extensionIndex = file.name.lastIndexOf('.')
+    const slicedFileName = file.name.slice(extensionIndex+1)
+    const acceptedExtensions = ['jpeg','jpg','png']
     const filePath = e.target.value.split(/\\/g)
     const fileName = filePath[filePath.length-1]
-    const formData = new FormData()
-    const email = JSON.parse(localStorage.getItem("user")).email
-    formData.append('file', file)
-    formData.append('email', email)
+    if(acceptedExtensions.includes(slicedFileName)) {
+      if(filePath) {
+        this.handleChangeFileBis(file, fileName)
+      }
+    } else {
+      this.alertExtension()
+      this.document.querySelector(`input[data-testid="file"]`).value = "";
+    }
+  }
 
-    this.store
-      .bills()
-      .create({
-        data: formData,
-        headers: {
-          noContentType: true
-        }
-      })
-      .then(({fileUrl, key}) => {
-        console.log(fileUrl)
-        this.billId = key
-        this.fileUrl = fileUrl
-        this.fileName = fileName
-      }).catch(error => console.error(error))
+  handleChangeFileBis = (file, fileName) => {
+    if(this.firestore) {
+      this.firestore
+          .storage
+          .ref(`justificatifs/${fileName}`)
+          .put(file)
+          .then(snapshot => snapshot.ref.getDownloadURL())
+          .then(url => {
+            this.fileUrl = url
+            this.fileName = fileName
+          })
+          .catch(error => error)
+    }
+
   }
   handleSubmit = e => {
     e.preventDefault()
@@ -57,20 +69,20 @@ export default class NewBill {
       fileName: this.fileName,
       status: 'pending'
     }
-    this.updateBill(bill)
+    this.createBill(bill)
     this.onNavigate(ROUTES_PATH['Bills'])
   }
 
   // not need to cover this function by tests
-  updateBill = (bill) => {
-    if (this.store) {
-      this.store
+  createBill = (bill) => {
+    if (this.firestore) {
+      this.firestore
       .bills()
-      .update({data: JSON.stringify(bill), selector: this.billId})
+      .add(bill)
       .then(() => {
         this.onNavigate(ROUTES_PATH['Bills'])
       })
-      .catch(error => console.error(error))
+      .catch(error => error)
     }
   }
 }
